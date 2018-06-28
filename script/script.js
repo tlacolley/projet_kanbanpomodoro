5309775de3ca60350f6f77514f9fa89a9852a3d8
tlacolley <thomaslacolley@gmail.com>
2018-06-28 12:30:14 +0200

    End function clik input, beginning drag n drop list

--- script/script.js
+++ script/script.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
 
 $(".buttonTask").click(function(){
 
-    var newTask=$("#newTask").get();
-    alert(newTask);
-    // $('#toDo').append('<li>');
-    // $('li').append();
-
+    var newTask = $("#newTask").val();
+    var list = $("<li>")
+    list.addClass("draggable")
+    $('#toDo').append(list);
+    $(list).append(newTask);
+    $("#newTask").val("");
 })
 
 
 
-})
+
+  $(".draggable").sortable();
+  $(".draggable").css({
+      "position":"absolute",
+      "cursor": "move",
+      "border": "solid 1px black",
+
+
+  });
+
+});
